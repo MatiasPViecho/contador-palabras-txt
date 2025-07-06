59dc259b37b087a03e07da7848d9f78597c01a12
MatiasPViecho <mpviecho@gmail.com>
2025-07-06 18:32:06 -0300

    add reader class and main test

--- src/contadorPalabrasTXT/Main.java
+++ src/contadorPalabrasTXT/Main.java
@@ -1,5 +1,9 @@
 package contadorPalabrasTXT;
 
 public class Main {
-
+	public static void main(String [] args) {
+		String path = "PATH_HERE";
+		Reader reader = new Reader(path);
+		reader.readFile();
+	}
 }
---
+++ src/contadorPalabrasTXT/Reader.java
@@ -0,0 +1,74 @@
+package contadorPalabrasTXT;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+public class Reader {
+	private static final int MAX_AMOUNT = 500;
+	private String path;
+	private int totalWords;
+	private boolean read;
+	private Map<String, Integer> wordFrecuency;
+	public Reader(String path) {
+		this.path = path;
+		this.totalWords = 0;
+		this.read = false;
+		this.wordFrecuency = new HashMap();
+	}
+	public void setPath(String path) {
+		if(!this.path.equals(path)) {
+			this.path = path;
+			this.setRead(false);
+		}
+	}
+	public void readFile() {
+		if(this.read) {
+			this.output();
+		}else {
+			this.tryReadFile();
+		}
+	}
+	private String getPath() {
+		return this.path;
+	}
+	private void tryReadFile() {
+		try(BufferedReader reader = new BufferedReader(new FileReader(this.getPath()))) {
+			String line;
+			while(((line = reader.readLine()) != null)) {
+				int idx = line.indexOf(" - ");
+				if(idx != -1) {
+					int msgStart = line.indexOf(": ", idx + 3);
+					if(msgStart != -1) {
+						String message = line.substring(msgStart + 2).trim().toLowerCase();
+						if (message.contains("multimedia omitido") || message.contains("mensaje omitido")) continue;
+						// quita de puntuación con regex
+						message.replaceAll("[^\\p{L}\\p{N}\\s]", "");
+						// Separación de palabras
+						String[] words = message.trim().split("\\s+");
+						for(String word: words) {
+							if(word.isEmpty()) continue;
+							this.sumeOneTotalWords();
+							wordFrecuency.put(word, wordFrecuency.getOrDefault(word, 0) + 1);
+						}
+						
+					}
+				}
+			}
+			this.setRead(true);
+			this.readFile();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private void setRead(boolean value) {
+		this.read = value;
+	}
+	private void sumeOneTotalWords() {
+		this.totalWords++;
+	}
+	private void output() {
+		System.out.println("Total de palabras: " + this.totalWords + "\nPalabras más frecuentes:\n");
+		wordFrecuency.entrySet().stream().sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue())).limit(MAX_AMOUNT).forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+	}
+}
