9ef96f66a384dd89c9270a99fe38db1ce846e8d5
MatiasPViecho <mpviecho@gmail.com>
2025-07-07 20:02:30 -0300

    add todos

--- src/contadorPalabrasTXT/Main.java
+++ src/contadorPalabrasTXT/Main.java
@@ -1,5 +1,14 @@
 package contadorPalabrasTXT;
 
+/**
+ * TODO: 
+ * - Herencia mal hecha, cambiar por patrón strategy o rever
+ * - Permitir seleccionar cantidad de palabras por consola
+ * - Permitir seleccionar archivo de texto por consola
+ * - Opción TXT default - TXT wsp (para utilizar o no los regex, -> enviar otra clase helper)
+ * - Ejecución de archivo
+ * - Interfaz gráfica?
+ */
 public class Main {
 	public static void main(String [] args) {
 		String path = "";
